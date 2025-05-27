51964f05fd087b19e6daf67ae8a08302747fa232
audreyrdsc <audreyrdsc@gmail.com>
2025-05-27 18:05:35 -0300

    Criação dos métodos para tratar os parâmetros de Report e Summary

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,23 +1,55 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SaleService {
 
 	@Autowired
 	private SaleRepository repository;
-	
+
+	//Busca por id
+	@Transactional(readOnly = true)
 	public SaleMinDTO findById(Long id) {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
+
+	//Busca conforme os parâmentros - gera paginação
+	@Transactional(readOnly = true)
+	public Page<SaleReportDTO> reportAll(String minDate, String maxDate, String name, Pageable pageable) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		LocalDate min = minDate.equals("") ? max.minusYears(1L) : LocalDate.parse(minDate);
+
+		Page<Sale> sales = repository.searchReport(min, max, name, pageable);
+		return sales.map(SaleReportDTO::new);
+	}
+
+	//Busca conforme os parâmentros - sem paginação (Lista)
+	@Transactional(readOnly = true)
+	public List<SaleSummaryDTO> getSummary(String minDate, String maxDate) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		LocalDate min = minDate.equals("") ? max.minusYears(1L) : LocalDate.parse(minDate);
+
+		return repository.searchSummary(min, max);
+	}
+
 }
