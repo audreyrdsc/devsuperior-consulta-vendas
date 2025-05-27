30ed98e5e927fd171a64e3a83e26111529fbc91a
audreyrdsc <audreyrdsc@gmail.com>
2025-05-27 18:03:40 -0300

    Criação dos métodos rota de Report e Summary

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,14 +1,16 @@
 package com.devsuperior.dsmeta.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.services.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -16,22 +18,34 @@ public class SaleController {
 
 	@Autowired
 	private SaleService service;
-	
+
+	//Rota pelo id
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<SaleMinDTO> findById(@PathVariable Long id) {
 		SaleMinDTO dto = service.findById(id);
 		return ResponseEntity.ok(dto);
 	}
 
+	//Rota por report (com ou sem argumentos - PAGINADA)
 	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
-		// TODO
-		return null;
+	public ResponseEntity<Page<SaleReportDTO>> getReport(
+			@RequestParam(defaultValue = "") String minDate,
+			@RequestParam(defaultValue = "") String maxDate,
+			@RequestParam(defaultValue = "") String name,
+			Pageable pageable) {
+
+		Page<SaleReportDTO> page = service.reportAll(minDate, maxDate, name, pageable);
+		return ResponseEntity.ok(page);
 	}
 
+	//Rota por summary (com ou sem argumentos - LISTADA)
 	@GetMapping(value = "/summary")
-	public ResponseEntity<?> getSummary() {
-		// TODO
-		return null;
+	public ResponseEntity<List<SaleSummaryDTO>> getSummary(
+			@RequestParam(defaultValue = "") String minDate,
+			@RequestParam(defaultValue = "") String maxDate) {
+
+		List<SaleSummaryDTO> list = service.getSummary(minDate, maxDate);
+		return ResponseEntity.ok(list);
 	}
+
 }
